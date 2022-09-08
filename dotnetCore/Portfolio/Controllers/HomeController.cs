932b667013a5ad189b6b89c24f73060f4642e383
chris.g.morton@elmutility.com <chris.g.morton@elmutility.com>
2022-09-08 16:45:01 -0600

    notes added

--- dotnetCore/Portfolio/Controllers/HomeController.cs
+++ dotnetCore/Portfolio/Controllers/HomeController.cs
@@ -9,9 +9,12 @@ namespace Portfolio
     public class HomeController : Controller
     {
         [HttpGet("")]
-        public string Index()
+        public ViewResult HiThere()
         {
-            return "Index";
+            //if no args then it will look for HiThere.cshtml first in the name of our controllers view directory 
+            // So for the HomeController it will look in the Home Folder in the Views Folder.  /Views/Home/HiThere.cshtml
+            // If it doesn't find it there it will look in /Views/Shared/HiThere.cshtml
+            return View();
         }
 
         [HttpGet("projects")]
