4f5e9eb993e8ea62bdd352bda7371d1f912efed0
chris.g.morton@elmutility.com <chris.g.morton@elmutility.com>
2022-09-09 09:12:16 -0600

    Razor Fun Item

--- dotnetCore/Portfolio/Controllers/HomeController.cs
+++ dotnetCore/Portfolio/Controllers/HomeController.cs
@@ -9,11 +9,18 @@ namespace Portfolio
     public class HomeController : Controller
     {
         [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet("HiThere")]
         public ViewResult HiThere()
         {
             //if no args then it will look for HiThere.cshtml first in the name of our controllers view directory 
             // So for the HomeController it will look in the Home Folder in the Views Folder.  /Views/Home/HiThere.cshtml
             // If it doesn't find it there it will look in /Views/Shared/HiThere.cshtml
+            // If there are 2 of the same named file it will grab the first one that it finds.
             return View();
         }
 
